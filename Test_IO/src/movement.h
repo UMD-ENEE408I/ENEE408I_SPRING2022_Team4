e0e55c4b0b22c260e22df49ae36404eaffbc89fc
Joe-Americo <98412187+Joe-Americo@users.noreply.github.com>
2022-02-08 11:29:36 -0500

    Arduino Movement Code

---
+++ Test_IO/src/movement.h
@@ -0,0 +1,17 @@
+#ifndef MOVEMENT_H 
+#define MOVEMENT_H
+
+void left_forward();
+void left_backward();
+void left_stop();
+
+void right_forward();
+void right_backward();
+void right_stop();
+
+void all_stop();
+void turn_left();
+void turn_right();
+void turn_around();
+
+#endif 
